ca69fe2fb33065298444a769bb749f5c99ba126f
caelinp <cpbluewhale@gmail.com>
2023-12-09 15:56:22 -0800

    cleaned up dec6

--- dec6/dec6.py
+++ dec6/dec6.py
@@ -1,16 +1,22 @@
-with open("dec6_input.txt", "r") as text:
-    data = text.readlines()
-    times = data[0].split()[1:]
-    distances = data[1].split()[1:]
-    print(times)
-    print(distances)
-    res = 1
-    for i in range(len(times)):
-        ways = 0
-        for holdTime in range(1, int(times[i])):
-            speed = holdTime
-            time = int(times[i])
-            if speed * (time - holdTime) > int(distances[i]):
-                ways += 1
-        res *= ways
-    print(res)
+import math
+
+def get_winning_range(time, distance):
+    # quadratic function: ax^2 + bx + c where x is time button is held down, a is 1, b is -time, c is the distance
+    sqrt_discriminant = math.sqrt(time**2 - 4*distance)
+
+    root1 = (time + sqrt_discriminant) / 2
+    root2 = (time - sqrt_discriminant) / 2
+    start = math.ceil(min(root1, root2))
+    end = math.floor(max(root1, root2))
+
+    return max(0, end - start + 1)
+
+times, distances = [val.split()[1:] for val in open("input.txt").read().split("\n")]
+res = [1, 0]
+for i in range(len(times)):
+    # part 1: product of all ways to win for all races:
+    res[0] *= get_winning_range(int(times[i]), int(distances[i]))
+
+# part 2: taking input as values for one race:
+res[1] = get_winning_range(int("".join(times)), int("".join(distances)))
+print("part 1: {}\npart 2: {}".format(*res))
