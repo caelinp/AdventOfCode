59651ddd8fb328eb4a471de133a1e06769e1dcde
caelinp <cpbluewhale@gmail.com>
2023-12-09 12:26:32 -0800

    ultimate one liner

--- dec9/dec9.py
+++ dec9/dec9.py
@@ -1,14 +1,12 @@
 input_file = "dec9_input.txt"
-histories = []
 
 def predict(pyramid):
     # construct difference pyramid
     while(any(pyramid[-1])):
         pyramid += [[pyramid[-1][i] - pyramid[-1][i - 1] for i in range(1, len(pyramid[-1]))]]
-    
+    # compute new first and last elements of each pyramid level and remove levels until 1 left. This will contain the a list of the two predicted values
     while len(pyramid) > 1:
-        pyramid[-2] = [pyramid[-2][0] - pyramid[-1][0]] + [pyramid[-1][-1] + pyramid[-2][-1]]
-        pyramid.pop()
+        pyramid = pyramid[:-2] + [[pyramid[-2][0] - pyramid[-1][0]] + [pyramid[-1][-1] + pyramid[-2][-1]]]
     return reversed(pyramid[0])
 
 with open(input_file, "r") as text:
