0acc15eaa87ced09787deebd77a8dd03ae01e32b
Caelin <prestoncaelin@gmail.com>
2023-12-10 21:33:46 -0800

    december 10 working but slow

--- dec10/dec10.py
+++ dec10/dec10.py
@@ -1,4 +1,4 @@
-
+import sys
 def get_origin(matrix):
     for row in range(len(matrix)):
         for col in range(len(matrix[0])):
@@ -9,9 +9,31 @@ def is_in_bounds(matrix, row, col):
     rows, cols = len(matrix), len(matrix[0])
     return (0 <= row < rows) and (0 <= col < cols)
 
+def fill_area(matrix, loop, value, row, col, outside_inside):
+    loop = set(loop)
+    symbol = "I" if value else "O"
+    def fill(row, col):
+        nonlocal value
+        if not is_in_bounds(matrix, row, col) or (row, col) in loop or (row, col) in outside_inside[0] or (row, col) in outside_inside[1]:
+            return
+        else:
+            outside_inside[value].add((row, col))
+            matrix[row][col] = symbol
+            north, east, south, west = get_north(row, col), get_east(row, col), get_south(row, col), get_west(row, col)
+            if north and north not in loop:
+                fill(*north)
+            if east and east not in loop:
+                fill(*east)
+            if south and south not in loop:
+                fill(*south)
+            if west and west not in loop:
+                fill(*west)
+    fill(row, col)
+    return outside_inside
+            
 def reset_origin_pipe(matrix, origin):
     row, col = origin
-    north, east, south, west = get_north(matrix, row, col, origin), get_east(matrix, row, col, origin), get_south(matrix, row, col, origin), get_west(matrix, row, col, origin)
+    north, east, south, west = get_north_pipe(matrix, row, col, origin), get_east_pipe(matrix, row, col, origin), get_south_pipe(matrix, row, col, origin), get_west_pipe(matrix, row, col, origin)
     if north and south:
         matrix[row][col] = "|"
     elif north and east:
@@ -25,20 +47,31 @@ def reset_origin_pipe(matrix, origin):
     elif east and west:
         matrix[row][col] = "-"
         
-
 def is_origin(matrix, row, col):
     return is_in_bounds(matrix, row, col) and matrix[row][col] == 'S'
 
-def get_north(matrix, row, col, last):
+def get_north(row, col):
+    return (row - 1, col)
+
+def get_east(row, col):
+    return (row, col + 1)
+
+def get_south(row, col):
+    return (row + 1, col)
+
+def get_west(row, col):
+    return (row, col - 1)
+
+def get_north_pipe(matrix, row, col, last):
     return (row - 1, col) if is_in_bounds(matrix, row - 1, col) and matrix[row - 1][col] in ['|','7','F', "S"] and (row - 1, col) != last else None
 
-def get_south(matrix, row, col, last):
+def get_south_pipe(matrix, row, col, last):
     return (row + 1, col) if is_in_bounds(matrix, row + 1 , col) and matrix[row + 1][col] in ['|','J','L', "S"] and (row + 1, col) != last else None
 
-def get_east(matrix, row, col, last):
+def get_east_pipe(matrix, row, col, last):
     return (row, col + 1) if is_in_bounds(matrix, row, col + 1) and matrix[row][col + 1] in ['-','7','J', "S"] and (row, col + 1) != last else None
 
-def get_west(matrix, row, col, last):
+def get_west_pipe(matrix, row, col, last):
     return (row, col - 1) if is_in_bounds(matrix, row, col - 1) and matrix[row][col - 1] in ['-','F','L', "S"] and (row, col - 1) != last else None
 
 def build_loop(matrix, origin):
@@ -47,7 +80,6 @@ def build_loop(matrix, origin):
     length = 0
     loop = [origin]
     while not is_origin(matrix, row, col):
-        #print("current:"+ str(row) + " " + str(col))
         loop.append((row, col))
         pos = get_next_pipe(matrix, row, col, last)
         last = (row, col)
@@ -55,72 +87,155 @@ def build_loop(matrix, origin):
         length += 1
     return loop, length + 1
 
-def get_loop_area(matrix, loop):
-    direction = None
-    for i in range(len(loop)):
-        loop
+def get_directions(matrix, row, col, last):
+    symbol = matrix[row][col]
+    # came from north
+    if last == (row - 1, col):
+        if symbol == "|":
+            return ["S"]
+        elif symbol == "L":
+            return ["S", "E"]
+        elif symbol == "J":
+            return ["S", "W"]
+    # came from south
+    elif last == (row + 1, col):
+        if symbol == "|":
+            return ["N"]
+        elif symbol == "7":
+            return ["N", "W"]
+        elif symbol == "F":
+            return ["N", "E"]
+    # came from west
+    elif last == (row, col - 1):
+        if symbol == "-":
+            return ["E"]
+        elif symbol == "7":
+            return ["E", "S"]
+        elif symbol == "J":
+            return ["E", "N"]
+    # came from east
+    elif last == (row, col + 1):
+        if symbol == "-":
+            return ["W"]
+        elif symbol == "F":
+            return ["W", "S"]
+        elif symbol == "L":
+            return ["W", "N"]
+    return None
+
+def get_loop_area(matrix, loop, outside_inside):
+    last = loop[0]
+    for (row, col) in loop[1:]:
+        pipe = matrix[row][col]
+        directions = get_directions(matrix, row, col, last)
+        north = get_north(row, col)
+        south = get_south(row, col)
+        east = get_east(row, col)
+        west = get_west(row, col)
+        if pipe == "|":
+            if directions == ["N"]:
+                fill_area(matrix, loop, 0, *west, outside_inside)
+                fill_area(matrix, loop, 1, *east, outside_inside)
+            elif directions == ["S"]:
+                fill_area(matrix, loop, 0, *east, outside_inside)
+                fill_area(matrix, loop, 1, *west, outside_inside)
+        elif pipe == "-":
+            if directions == ["E"]:
+                fill_area(matrix, loop, 1, *south, outside_inside)
+                fill_area(matrix, loop, 0, *north, outside_inside)
+            elif directions == ["W"]:
+                fill_area(matrix, loop, 1, *north, outside_inside)
+                fill_area(matrix, loop, 0, *south, outside_inside)
+        elif pipe == "L":
+            if directions == ["S", "W"]:
+                fill_area(matrix, loop, 1, *south, outside_inside)
+                fill_area(matrix, loop, 1, *west, outside_inside)
+            elif directions == ["E", "N"]:
+                fill_area(matrix, loop, 0, *south, outside_inside)
+                fill_area(matrix, loop, 0, *west, outside_inside)
+        elif pipe == "7":
+            if directions == ["E", "S"]:
+                fill_area(matrix, loop, 0, *north, outside_inside)
+                fill_area(matrix, loop, 0, *east, outside_inside)
+            elif directions == ["N", "W"]:
+                fill_area(matrix, loop, 1, *north, outside_inside)
+                fill_area(matrix, loop, 1, *east, outside_inside)
+        elif pipe == "J":
+            if directions == ["S", "W"]:
+                fill_area(matrix, loop, 0, *south, outside_inside)
+                fill_area(matrix, loop, 0, *east, outside_inside)
+            elif directions == ["E", "N"]:
+                fill_area(matrix, loop, 1, *south, outside_inside)
+                fill_area(matrix, loop, 1, *east, outside_inside)
+        elif pipe == "F":
+            if directions == ["N", "E"]:
+                fill_area(matrix, loop, 0, *north, outside_inside)
+                fill_area(matrix, loop, 0, *west, outside_inside)
+            elif directions == ["W", "S"]:
+                fill_area(matrix, loop, 1, *north, outside_inside)
+                fill_area(matrix, loop, 1, *west, outside_inside)
+
+        last = (row, col)
 
 
-# this will go clockwise
+
 def get_next_pipe(matrix, row, col, last):
-    pipe = matrix[row][col]
     if is_origin(matrix, row, col):
-        if (north := get_north(matrix, row, col, last)):
+        if (north := get_north_pipe(matrix, row, col, last)):
             return north
-        elif (east := get_east(matrix, row, col, last)):
+        elif (east := get_east_pipe(matrix, row, col, last)):
             return east
-        elif (south := get_south(matrix, row, col, last)):
+        elif (south := get_south_pipe(matrix, row, col, last)):
             return south
-        elif (west := get_west(matrix, row, col, last)):
+        elif (west := get_west_pipe(matrix, row, col, last)):
             return west
+    pipe = matrix[row][col]
     if pipe == "|":
-        if (north := get_north(matrix, row, col, last)):
+        if (north := get_north_pipe(matrix, row, col, last)):
             return north
-        elif (south := get_south(matrix, row, col, last)):
+        elif (south := get_south_pipe(matrix, row, col, last)):
             return south 
     elif pipe == "-":
-        get_west(matrix, row, col, last)
-        if (east := get_east(matrix, row, col, last)):
-
+        get_west_pipe(matrix, row, col, last)
+        if (east := get_east_pipe(matrix, row, col, last)):
             return east
-        elif (west := get_west(matrix, row, col, last)):
+        elif (west := get_west_pipe(matrix, row, col, last)):
             return west
-        
     elif pipe == "L":
-        if (north := get_north(matrix, row, col, last)):
+        if (north := get_north_pipe(matrix, row, col, last)):
             return north
-        elif (east := get_east(matrix, row, col, last)):
+        elif (east := get_east_pipe(matrix, row, col, last)):
             return east
     elif pipe == "F":
-        if (east := get_east(matrix, row, col, last)):
+        if (east := get_east_pipe(matrix, row, col, last)):
             return east
-        elif (south := get_south(matrix, row, col, last)):
+        elif (south := get_south_pipe(matrix, row, col, last)):
             return south 
     elif pipe == "7":
-        if (west := get_west(matrix, row, col, last)):
+        if (west := get_west_pipe(matrix, row, col, last)):
             return west
-        elif (south := get_south(matrix, row, col, last)):
+        elif (south := get_south_pipe(matrix, row, col, last)):
             return south 
     elif pipe == "J":
-        if (north := get_north(matrix, row, col, last)):
+        if (north := get_north_pipe(matrix, row, col, last)):
             return north
-        elif (west := get_west(matrix, row, col, last)):
+        elif (west := get_west_pipe(matrix, row, col, last)):
             return west
     return None
 
-matrix = [list(line) for line in open("example_input.txt").read().split("\n")]
+sys.setrecursionlimit(10000)
+
+matrix = [list(line) for line in open("input.txt").read().split("\n")]
 origin = get_origin(matrix)
 
 loop, length = build_loop(matrix, origin)
 farthest = length // 2
 
 reset_origin_pipe(matrix, origin)
+outside_inside = [set(), set()]
 
-
-print(loop)
-area = get_loop_area(matrix, loop)
-for line in matrix:
-    print("".join(line))
+get_loop_area(matrix, loop, outside_inside)
+area = min(len(outside_inside[0]), len(outside_inside[1]))
 print("part 1: {}\npart 2: {}".format(farthest, area))
 
 
