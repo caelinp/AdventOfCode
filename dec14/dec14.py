e3f97b07f9826f528db6661b1ee211a9a9a2a9ef
caelinp <cpbluewhale@gmail.com>
2023-12-14 12:20:32 -0800

    cleaned up dec14

--- dec14/dec14.py
+++ dec14/dec14.py
@@ -1,6 +1,3 @@
-from functools import cache
-
-@cache
 def get_north_load_after_north_tilt(rocks):
     rocks = rocks.split("\n")
     load = 0
@@ -14,7 +11,6 @@ def get_north_load_after_north_tilt(rocks):
                 weight = len(rocks) - i - 1
     return load
 
-@cache 
 def get_north_load(rocks):
     rocks = rocks.split("\n")
     load = 0
@@ -24,7 +20,6 @@ def get_north_load(rocks):
                 load += len(rocks) - i
     return load
 
-@cache
 def tilt_north(rocks):
     rocks = [list(row) for row in rocks.split("\n")]
     for j in range(len(rocks[0])):
@@ -39,7 +34,6 @@ def tilt_north(rocks):
                 new_row = i + 1
     return "\n".join(["".join(row) for row in rocks])
 
-@cache
 def tilt_south(rocks):
     rocks = [list(row) for row in rocks.split("\n")]
     for j in range(len(rocks[0])):
@@ -54,7 +48,6 @@ def tilt_south(rocks):
                 new_row = i - 1
     return "\n".join(["".join(row) for row in rocks])
 
-@cache
 def tilt_west(rocks):
     rocks = [list(row) for row in rocks.split("\n")]
     for i in range(len(rocks)):
@@ -69,7 +62,6 @@ def tilt_west(rocks):
                 new_col = j + 1
     return "\n".join(["".join(row) for row in rocks])
 
-@cache
 def tilt_east(rocks):
     rocks = [list(row) for row in rocks.split("\n")]
     for i in range(len(rocks)):
@@ -84,7 +76,6 @@ def tilt_east(rocks):
                 new_col = j - 1
     return "\n".join(["".join(row) for row in rocks])
 
-@cache
 def cycle(rocks):
     rocks = tilt_north(rocks)
     rocks = tilt_west(rocks)
@@ -95,25 +86,22 @@ def cycle(rocks):
 rocks = open("input.txt").read()
 
 p1 = get_north_load_after_north_tilt(rocks)
-p2 = 0
-loads = []
-history = {}
-for i in range(300):
-    rocks = cycle(rocks)
+rock_patterns = {} # mapping of all rock patterns (as strings) to the idx at which they were reached
+loads = [] # list of north load values at every idx
+
+cycle_idx = 0
+while rocks not in rock_patterns:
     loads.append(get_north_load(rocks))
+    rock_patterns[rocks] = cycle_idx
+    rocks = cycle(rocks)
+    cycle_idx += 1
 
-    # check for repetition cycle
-    if i > 20:
-        state_hash = str(loads[-20:])
-        if state_hash in history:
-            rep_cycle_start = history[state_hash]
-            rep_cycle_length = i - rep_cycle_start
-            break
-        history[state_hash] = i
+loop_start = rock_patterns[rocks]
+loop_length = len(rock_patterns) - loop_start
+offset = (1_000_000_000 - loop_start) % loop_length
 
-target = 1_000_000_000
-offset = (target - rep_cycle_start) % rep_cycle_length - 1  # -1 because initial load was not recorded 
-p2 = loads[rep_cycle_start + offset]
+p2 = loads[loop_start + offset]
+
 
 print("part 1: {}\npart 2: {}".format(p1, p2))
 
