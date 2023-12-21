b6613c32ca1bf431f3f5ea82bfad48faad0c570f
Caelin <prestoncaelin@gmail.com>
2023-12-21 11:03:28 -0800

    cleaned up de21 code

--- 2023/dec21/dec21.py
+++ 2023/dec21/dec21.py
@@ -5,11 +5,9 @@ def get_start(garden):
                 return r, c
             
 def get_plots_from_steps(garden, steps):
-    height = len(garden)
-    width = len(garden[0])
-    start = get_start(garden)
-    plots = {start}
-    points = {}
+    height, width = len(garden), len(garden[0])
+    plots = {get_start(garden)}
+    points = []
     for step in range(1, steps):
         new_plots = set()
         for plot in plots:
@@ -22,18 +20,16 @@ def get_plots_from_steps(garden, steps):
         if step == 64:
             p1 = len(plots)
         if step % width == steps % width:
-            points[step // width] = len(plots)
+            points.append(len(plots))
         if len(points) == 3:
             break
     def f(n):
-        y0 = points[0]
-        y1 = points[1]
-        y2 = points[2]
-        a = (y2+y0-2*y1)/2
-        b = y1-y0 -a
+        y0, y1, y2 = points
+        a = (y2 + y0 - 2 * y1) / 2
+        b = y1 - y0 - a
         c = y0
-        return int(a*n**2 + b*n +c )
-    p2 = f(steps//width)
+        return int(a * n ** 2 + b * n + c )
+    p2 = f(steps // width)
     return p1, p2
 # Load the garden from the file
 garden = [[plot for plot in row] for row in open('input.txt').read().splitlines()]
